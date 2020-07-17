fa99a665ddffa4870012bb67de5aa1fbc3bb3dd5
vincent bonnier <vincent@kollebolle.com>
2020-07-17 11:32:54 +0200

    mise en place twig

--- index.php
+++ index.php
@@ -1,8 +1,33 @@
 <?php
+    //Loading composer 
     require 'vendor/autoload.php'; 
 
-    use App\entity\Contact;
+    //configuration de notre moteur de template
+	use Twig\Environment;
+    use Twig\Loader\FilesystemLoader;
     
-    $contact = new Contact("bonnier","vincent");
-    echo $contact->sayMyName(); 
+    //creation moteur template
+    $loader = new FilesystemLoader('app/template');
+	$twig = new Environment($loader);
+	
+    //we can use contact without include the class, it's autoloaded.
+    //use App\entity\Contact;
+    //$contact1 = new Contact("pikachu","vincent");
+    
+    $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+	
+	// route the request internally
+	if ('/index.php' === $uri || '/formation/' === $uri) {
+		echo $twig->render('index.html.twig');
+	} elseif ('/index.php/show' === $uri && isset($_GET['id'])) {
+		echo "show_action";
+	} elseif ('/index.php/create' === $uri) {
+		echo "create_action";
+	} elseif ('/index.php/update' === $uri && isset($_GET['id'])) {
+		echo "update_action";
+	} else {
+		header('HTTP/1.1 404 Not Found');
+		echo '<html><body><h1>Page Not Found</h1></body></html>';
+	}
+	
     
